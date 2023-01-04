00e6623811ecdb2fe629f563d0884dd32aa6097e
Czarek Polak <cezary.polak@dac.digital>
2023-01-04 13:47:59 +0100

    Script allows to run the k-fold training with specific parameters Can be use with cron to schedule the run on the machine. I.e to run it at 4th Jan 9:45 modify the paths to data env in `k-fold-scheduler.sh` and also params of the python script. Next call `crontab -e` and type `45 09 04 01 path-to-script/k-fold-scheduler.sh`

---
+++ k-fold-scheduler.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# set a path to venv on the machine
+ENV_PATH="/home/czarek/envs/web-eye-tracking-algo"
+DIRECTORY_PATH="/home/czarek/Documents/work/web-eye-tracking-algo/code/third_party/L2CS-Net"
+DATA_PATH="/home/czarek/Downloads/demo_dataset_3/train"
+activate () {
+    . $ENV_PATH/bin/activate
+}
+
+activate
+
+# set the parameters of the k-fold training
+python $DIRECTORY_PATH/clear_kfold_hydrant_training.py \
+--train-dir $DATA_PATH \
+--architecture efficientnet_b3 \
+--batch-size 2 \
+--clearml-experiment local_experimet_01 \
+--clearml-tags kfold local test scheduled \
+--epochs 5 \
+--k-fold 5 \
+--lr 1e-05 \
+--pitch-cls-scale 1.0 \
+--pitch-lower-range -42 \
+--pitch-reg-scale 1.0 \
+--pitch-resolution 3 \
+--pitch-upper-range 42 \
+--smoothing-sigma 0.45 \
+--smoothing-threshold 0.001 \
+--yaw-cls-scale 1.0 \
+--yaw-lower-range -42 \
+--yaw-reg-scale 1.0 \
+--yaw-resolution 3 \
+--yaw-upper-range 42
+
+deactivate
